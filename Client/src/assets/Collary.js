2a42bb2aef5183fb1ab6c129a7381c68d1a47870
OM3X4 <om3x4e@gmail.com>
2025-01-22 17:50:40 +0200

    started working in the collory

---
+++ Client/src/assets/Collary.js
@@ -0,0 +1,19 @@
+
+/* eslint-disable */
+const data = {
+    "1" : [
+        {numbers:"" ,  text: "In The First Trick We Are Going To use Rounding"},
+        {   
+            numbers:"47 + 29" , 
+            text:"In Question Like We Can Round 29 to be 30 then add 30 to 47 which will easily be 77 then subtract the one leaving us with 76 as our answer"
+            ,answer:false,
+            draft:false,
+        },
+        {numbers:"123 + 78" ,
+            text:"So in This Example Which number will be rounded",
+            answer:{choices:["123" , "78"] , answer:"78"},
+            draft:False
+        }
+    ]
+    
+}
